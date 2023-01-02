255227e246b6c272782ecb8d64c68855d59057d9
Suyog Subedi <subedisuyog555@gmail.com>
2023-01-02 13:17:18 +0545

    database connection added

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const express = require("express")
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const registerVehicle = require("./src/routes/vehicleRegistration.route")
+require('dotenv').config()
+app.use(express.json())
+
+// database connection
+try {
+    mongoose.connect(process.env.MONGODB_URI).then(() => {
+      console.log("Connected to the database successfully")
+      port = process.env.PORT
+      app.listen(port, () => {
+        console.log(`VCSMS Backend Listening At Port: ${port}`);
+        console.log(`http://localhost:${port}`);
+      });
+    });
+  } catch {
+    console.log("Not Connected");
+  }
+
 app.get("/",(req,res)=>{
     res.json("Hello From VCSMS Backend")
 })
-app.listen(port,()=>{
-    console.log("server running on port",port)
-})
+app.use("/register",registerVehicle)
