78ea7a11ae8835fc54e13042a186f05d27ca7963
Suyog Subedi <subedisuyog555@gmail.com>
2023-01-13 15:41:45 +0545

    removed 400 status code for the username from signup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,7 +27,7 @@ const signupUser = async (req, res) => {
     const token = createToken(user._id);
     res.status(200).json({ username, token });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.json({ error: error.message });
   }
 };
 
